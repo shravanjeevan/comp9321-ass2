22bc0a2e3151c0eaf2e0da54be901969ecfa1f79
Mirette <mira.saleh@hotmail.com>
2019-11-12 22:26:27 +1100

    Added skeleton HTML files

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return 'hi'
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
